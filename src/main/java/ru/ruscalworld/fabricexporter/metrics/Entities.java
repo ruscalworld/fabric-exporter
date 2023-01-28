07e2f576d1e6586b7e25254c92af1358de05ed41
ruscalworld <me@ruscal.world>
2023-01-28 13:43:20 +0300

    Migrate to Minecraft 1.19.3

--- src/main/java/ru/ruscalworld/fabricexporter/metrics/Entities.java
+++ src/main/java/ru/ruscalworld/fabricexporter/metrics/Entities.java
@@ -2,10 +2,10 @@ package ru.ruscalworld.fabricexporter.metrics;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypeFilter;
-import net.minecraft.util.registry.Registry;
 import ru.ruscalworld.fabricexporter.FabricExporter;
 import ru.ruscalworld.fabricexporter.util.TextUtil;
 
@@ -35,14 +35,14 @@ public class Entities extends Metric {
             HashMap<String, Integer> currentWorldEntities = new HashMap<>();
 
             world.getEntitiesByType(ENTITY_FILTER, (entity) -> true).forEach(entity -> {
-                String name = Registry.ENTITY_TYPE.getId(entity.getType()).getPath();
+                String name = Registries.ENTITY_TYPE.getId(entity.getType()).getPath();
                 Integer typeCount = currentWorldEntities.getOrDefault(name, 0);
                 currentWorldEntities.put(name, typeCount + 1);
             });
 
             for (String type : currentWorldEntities.keySet()) {
                 Integer count = currentWorldEntities.get(type);
-                EntityType<?> entityType = Registry.ENTITY_TYPE.get(new Identifier(type));
+                EntityType<?> entityType = Registries.ENTITY_TYPE.get(new Identifier(type));
                 this.getGauge().labels(TextUtil.getWorldName(world), entityType.getSpawnGroup().getName(), type).set(count);
             }
         }
